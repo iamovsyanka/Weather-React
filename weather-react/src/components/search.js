7cb4ef2322894e8b6856103c26425e667c1302af
AnnaKostyukova <annakostyukova@coherentsolutions.com>
2022-03-20 22:18:21 +0300

    implenent react example

---
+++ weather-react/src/components/search.js
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+
+import "../static/css/search.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Search = (props) => {
+    const [cityName, setCityName] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const onChangeCity = (e) => {
+        const cityName = e.target.value;
+        setCityName(cityName)
+        setErrorMessage(() => cityName.match(/\d+/) ? "City is invalid" : '')
+    }
+
+    const search = async (e) => {
+        e.preventDefault();
+
+        if (errorMessage === '') {
+            props.setCity(cityName);
+        }
+    }
+
+    return (
+        <form>
+            <input
+                type="text"
+                className="search-bar"
+                placeholder="Enter city..."
+                onChange={onChangeCity}
+                value={cityName}
+            />
+            <button type="submit" className="button search-button" onClick={search}>
+                Search
+            </button>
+            {errorMessage ? (
+                <div className="alert alert-danger" role="alert">
+                    {errorMessage}
+                </div>
+            ) : ('')}
+        </form>
+    );
+}
+
+export default Search;
---
+++ weather-react/src/components/weather.js
@@ -0,0 +1,66 @@
+import React, {useState} from "react";
+
+import weatherService from "../services/weatherService";
+import Search from "./search";
+import '../static/css/weather.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Weather = () => {
+    const [weather, setWeather] = useState({});
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const setCity = async (cityName) => {
+        let response = await weatherService.getWeatherByCityName(cityName);
+
+        if (response.main) {
+            setWeather(response);
+            setErrorMessage("");
+        } else {
+            setWeather("");
+            setErrorMessage(response.message);
+        }
+    }
+
+    const dateBuilder = (d) => {
+        let options = {
+            weekday: "long", year: "numeric", month: "long",
+            day: "numeric", hour: "2-digit", minute: "2-digit"
+        };
+
+        return d.toLocaleTimeString("en-us", options)
+    }
+
+    const tempBuilder = (t) => Math.round(t) + '°c';
+
+    return (
+        <div className='app'>
+            <main>
+                <div className="search-box">
+                    <Search setCity={setCity}/>
+                </div>
+                {weather.main ? (
+                    <div>
+                        <div className="location-box">
+                            <div className="location">{weather.name}, {weather.sys.country}</div>
+                            <div className="date">{dateBuilder(new Date())}</div>
+                        </div>
+                        <div className="weather-box">
+                            <div className="temp">
+                                {tempBuilder(weather.main.temp)}
+                            </div>
+                            <div className="weather">{weather.weather[0].main}</div>
+                        </div>
+                        <div className="map"/>
+                    </div>
+                ) : ('')}
+                {errorMessage ? (
+                    <div className="alert alert-danger" role="alert">
+                         {errorMessage}
+                    </div>
+                ) : ('')}
+            </main>
+        </div>
+    );
+}
+
+export default Weather;
